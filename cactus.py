406cee8b227e41f53f28cb66a2698b709f144819
emi~ <esauvisky@gmail.com>
2023-11-09 07:57:10 -0300

    feat: increase git diff context size from 3 to 5 lines in staging area

--- cactus.py
+++ cactus.py
@@ -146,7 +146,7 @@ def get_git_diff(context_size):
     # cmd += f"--ignore-submodules --ignore-space-at-eol --minimal --no-color --no-ext-diff --no-indent-heuristic --no-textconv --no-renames --unified={context_size}"
 
     # cmd += "--ignore-submodules --ignore-space-at-eol --minimal --no-color --no-ext-diff --no-indent-heuristic --no-textconv "
-    cmd = f"git diff --inter-hunk-context={context_size} -U3 --minimal -p --staged"
+    cmd = f"git diff --inter-hunk-context={context_size} -U5 --minimal -p --staged"
     # cmd += f"--unified=3"
     result = run(cmd)
     if result.returncode != 0:
